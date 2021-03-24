5b7819734373cc1f6522377585ede9d2e7b0c4cb
Konstantin Lepeshenkov <kolepes@microsoft.com>
2021-03-24 18:27:52 +0100

    autogenerating the boilerplate

--- CounterEntity/index.ts
+++ CounterEntity/index.ts
@@ -1,40 +1,5 @@
+
 import * as DurableFunctions from "durable-functions"
-
-import { DurableEntity, VisibilityEnum } from '../Common/DurableEntity'
-import { CounterState } from '../ui/src/shared/CounterState';
-
-// Sample counter entity
-class CounterEntity extends DurableEntity<CounterState>
-{
-    // This method is async just to demonstrate, that your entity does can have both sync and async methods
-    async add(value: number) {
-        this.state.history.unshift(this.state.countContainer.count);
-        this.state.history.splice(10, 1);
-
-        this.state.countContainer.count += value;
-    }
-
-    substract(value: number) {
-        this.state.history.unshift(this.state.countContainer.count);
-        this.state.history.splice(10, 1);
-
-        this.state.countContainer.count -= value;
-    }
-
-    // Overriding visibility
-    protected get visibility(): VisibilityEnum { return VisibilityEnum.ToEveryone; }
-
-    // Custom state initialization for a newly created entity
-    protected initializeState(): CounterState {
-
-        var newState = new CounterState();
-
-        newState.countContainer.count = 1;
-        newState.title = `Counter-${new Date().toISOString()}`;
-
-        return newState;
-    }
-}
-
-// Boilerplate to expose CounterEntity as a Durable Entity
+import { CounterEntity } from '../DurableEntities/CounterEntity';
 export default DurableFunctions.entity((ctx) => new CounterEntity(ctx).handleSignal());
+        
