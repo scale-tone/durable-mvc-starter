51a58e8ed75e84bd7fd6cab6a5a9c24c5223b5ed
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2021-05-11 16:39:14 +0300

    more tests

--- __test/ui/src/common/DurableEntitySet.test.ts
+++ __test/ui/src/common/DurableEntitySet.test.ts
@@ -1,4 +1,5 @@
 
+import { assert } from 'node:console';
 import { DurableEntitySet } from '../../../../ui/src/common/DurableEntitySet';
 import { BackendBaseUri } from '../../../../ui/src/common/DurableHttpClient';
 import { DurableEntityClientStateContainer } from '../../../../ui/src/shared/common/DurableEntityClientStateContainer';
@@ -64,12 +65,6 @@ test('creates an entity by initializing its metadata', async () => {
     const initialFieldValue = 'value1';
     const fetchedFieldValue = 'value2';
 
-    const clientStateContainer = <DurableEntityClientStateContainer>{
-        version: 1,
-        state: {
-            someField: fetchedFieldValue
-        }
-    };
 
     (DurableEntitySet as any).SignalRConn = undefined;
 
@@ -89,7 +84,12 @@ test('creates an entity by initializing its metadata', async () => {
         get: (url) => {
             getUrls.push(url);
             return Promise.resolve({
-                content: JSON.stringify(clientStateContainer)
+                content: JSON.stringify({
+                    version: 1,
+                    state: {
+                        someField: fetchedFieldValue
+                    }
+                })
             });
         }
     };
@@ -119,13 +119,16 @@ test('creates an entity by initializing its metadata', async () => {
     expect((DurableEntitySet as any).SignalRConn.connection.httpClient).toBe(fakeHttpClient);
 });
 
-test('reconnects to SignalR', async () => {
+test('reconnects to SignalR and attaches entities', async () => {
 
     // arrange
 
     (DurableEntitySet as any).SignalRConn = undefined;
 
     const postUrls = [], getUrls = [];
+
+    const entityKey = 'mykey';
+    const fetchedFieldValue = 'some value';
 
     const fakeHttpClient = {
 
@@ -140,7 +143,15 @@ test('reconnects to SignalR', async () => {
 
         get: (url) => {
             getUrls.push(url);
-            return Promise.resolve([]);
+            return Promise.resolve({
+                content: JSON.stringify([{
+                    version: 1,
+                    entityKey,
+                    state: {
+                        someField: fetchedFieldValue
+                    }
+                }])
+            });
         }
     };
 
@@ -148,9 +159,11 @@ test('reconnects to SignalR', async () => {
 
     // act
 
-    new DurableEntitySet('myentity', true);
+    const entitySet = new DurableEntitySet('myentity', true);
 
     // Assert
+
+    await new Promise(resolve => setTimeout(resolve, 10));
 
     const signalRConn = (DurableEntitySet as any).SignalRConn;
     const onCloseCallback = signalRConn.closedCallbacks[0];
@@ -162,4 +175,85 @@ test('reconnects to SignalR', async () => {
 
     expect(getUrls.length).toBe(1);
     expect(getUrls[0]).toBe('http://localhost:7071/a/p/i/entities/myentity');
+
+    expect(entitySet.items.length).toBe(1);
+    const entityState = entitySet.items[0];
+    expect(entityState.entityKey).toBe(entityKey);
+    expect((entityState as any).someField).toBe(fetchedFieldValue);
+});
+
+test('applies entity state change', async () => {
+
+    // arrange
+
+    (DurableEntitySet as any).SignalRConn = undefined;
+
+    const postUrls = [], getUrls = [];
+
+    const entityName = 'myentity3';
+    const entityKey = 'mykey3';
+    const fetchedFieldValue = 'value3';
+    const deliveredFieldValue = 'value4';
+    var fetchedVersion = 1;
+
+    const fakeHttpClient = {
+
+        send: (url) => {
+            throw new Error('Should not be used');
+        },
+
+        post: (url) => {
+            postUrls.push(url);
+            return Promise.resolve();
+        },
+
+        get: (url) => {
+            getUrls.push(url);
+            return Promise.resolve({
+                content: JSON.stringify({
+                    version: fetchedVersion,
+                    state: {
+                        someField: fetchedFieldValue
+                    }
+                })
+            });
+        }
+    };
+
+    (DurableEntitySet as any).HttpClient = fakeHttpClient;
+
+    const stateChangedMessage1 = {
+        entityName,
+        entityKey,
+        version: 2,
+        stateDiff: [
+            { op: 'replace', path: '/someField', value: deliveredFieldValue }
+        ]
+    };
+
+    const stateChangedMessage2 = {
+        entityName,
+        entityKey,
+        version: 123
+    };
+
+    // act
+
+    const observableState = DurableEntitySet.attachEntity(entityName, entityKey, { someField: 'initial value' });
+    await new Promise(resolve => setTimeout(resolve, 10));
+    (DurableEntitySet as any).entityStateChangedMessageHandler(stateChangedMessage1);
+
+    // Assert
+
+    expect(observableState.someField).toBe(deliveredFieldValue);
+
+    // act
+
+    fetchedVersion = 123;
+    (DurableEntitySet as any).entityStateChangedMessageHandler(stateChangedMessage2);
+    await new Promise(resolve => setTimeout(resolve, 10));
+
+    // Assert
+
+    expect(observableState.someField).toBe(fetchedFieldValue);
 });
