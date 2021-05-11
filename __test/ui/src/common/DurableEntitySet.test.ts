acc641ec8a518a3e78c52b234c35cba88575414c
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2021-05-11 18:12:19 +0300

    more tests

--- __test/ui/src/common/DurableEntitySet.test.ts
+++ __test/ui/src/common/DurableEntitySet.test.ts
@@ -1,8 +1,6 @@
 
-import { assert } from 'node:console';
 import { DurableEntitySet } from '../../../../ui/src/common/DurableEntitySet';
 import { BackendBaseUri } from '../../../../ui/src/common/DurableHttpClient';
-import { DurableEntityClientStateContainer } from '../../../../ui/src/shared/common/DurableEntityClientStateContainer';
 
 // Need to set it to an absolute URL to calm down SignalR's HttpConnection
 (BackendBaseUri as any) = 'http://localhost:7071/a/p/i';
@@ -239,21 +237,123 @@ test('applies entity state change', async () => {
 
     // act
 
+    // Attaching an entity, so that its state is fetched from server
     const observableState = DurableEntitySet.attachEntity(entityName, entityKey, { someField: 'initial value' });
     await new Promise(resolve => setTimeout(resolve, 10));
+    
+    // Simulating an EntityStateChangedMessage with some state changes in it
     (DurableEntitySet as any).entityStateChangedMessageHandler(stateChangedMessage1);
 
-    // Assert
+    const fieldValue1 = observableState.someField;
 
-    expect(observableState.someField).toBe(deliveredFieldValue);
-
-    // act
-
+    // Now simulating another EntityStateChangedMessage, with unexpected version - this should trigger
+    // another state fetch from server
     fetchedVersion = 123;
     (DurableEntitySet as any).entityStateChangedMessageHandler(stateChangedMessage2);
     await new Promise(resolve => setTimeout(resolve, 10));
 
+    const fieldValue2 = observableState.someField;
+
     // Assert
 
-    expect(observableState.someField).toBe(fetchedFieldValue);
+    expect(fieldValue1).toBe(deliveredFieldValue);
+    expect(fieldValue2).toBe(fetchedFieldValue);
+});
+
+
+test('drops destroyed entity from entity collection', async () => {
+
+    // arrange
+
+    (DurableEntitySet as any).SignalRConn = undefined;
+
+    const postUrls = [], getUrls = [];
+
+    const entityName = 'myentity4';
+    const entityKey = 'mykey4';
+
+    const fakeHttpClient = {
+
+        send: (url) => {
+            throw new Error('Should not be used');
+        },
+
+        post: (url) => {
+            postUrls.push(url);
+            return Promise.resolve();
+        },
+
+        get: (url) => {
+            getUrls.push(url);
+            return Promise.resolve({
+                content: JSON.stringify([{
+                    version: 1,
+                    entityKey,
+                    state: {}
+                }])
+            });
+        }
+    };
+
+    (DurableEntitySet as any).HttpClient = fakeHttpClient;
+
+    const stateChangedMessage = {
+        entityName,
+        entityKey,
+        version: 2,
+        isEntityDestructed: true
+    };
+
+    // act
+
+    const entitySet = new DurableEntitySet(entityName, true);
+    await new Promise(resolve => setTimeout(resolve, 10));
+
+    expect(entitySet.items.length).toBe(1);
+    expect(entitySet.items[0].entityKey).toBe(entityKey);
+
+    (DurableEntitySet as any).entityStateChangedMessageHandler(stateChangedMessage);
+
+    // Assert
+    expect(entitySet.items.length).toBe(0);
+});
+
+test('retries fetching the state from server', async () => {
+
+    // arrange
+
+    (DurableEntitySet as any).SignalRConn = undefined;
+
+    const getUrls = [];
+
+    const fakeHttpClient = {
+
+        send: (url) => {
+            throw new Error('Should not be used');
+        },
+
+        get: (url) => {
+            getUrls.push(url);
+            return Promise.resolve({
+                statusCode: 500
+            });
+        }
+    };
+
+    (DurableEntitySet as any).HttpClient = fakeHttpClient;
+
+    (DurableEntitySet as any).RetryBaseIntervalMs = 0;
+
+    // act
+
+    DurableEntitySet.attachEntity('myentity5', 'mykey5', { someField: 'some value' });
+    await new Promise(resolve => setTimeout(resolve, 100));
+
+    // Assert
+
+    expect(getUrls.length).toBe(7);
+    expect(getUrls[0]).toBe('http://localhost:7071/a/p/i/entities/myentity5/mykey5');
+    for (var i = 1; i < 7; i++){
+        expect(getUrls[i]).toBe(getUrls[i-1]);
+    }
 });
